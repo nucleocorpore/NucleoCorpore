dbc93d55a83e34cb6bbca258986870b4e6a1b316
Gustavo Castro <gstvcastroc@gmail.com>
2026-03-05 17:13:08 -0300

    feat: implementacao de icone flutuante e ajuste de secoes

--- js/site.js
+++ js/site.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("ano").textContent = new Date().getFullYear();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const numero = "5531972479061";
+    const mensagem = "Olá! Vim pelo site e gostaria de agendar uma aula experimental.";
+
+    const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+    document.getElementById("whatsapp-float").href = link;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    const numero = "5531972479061";
+    const mensagem = "Olá! Vim pelo site e gostaria de agendar uma aula experimental.";
+
+    const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`
+    document.getElementById("whatsapp-float").href = link
+})
