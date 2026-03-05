40b962d1d1964d541920cb04a20b6c2a081f9a30
Gustavo Castro <gstvcastroc@gmail.com>
2026-03-05 17:13:57 -0300

    fix: ajuste js duplicado

--- js/site.js
+++ js/site.js
@@ -3,16 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-
-    const numero = "5531972479061";
-    const mensagem = "Olá! Vim pelo site e gostaria de agendar uma aula experimental.";
-
-    const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
-
-    document.getElementById("whatsapp-float").href = link;
-});
-
-document.addEventListener("DOMContentLoaded", function () {
     const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
